803e5218534848a326618aa8a584a68f0419c633
denjasan <denjasan@gmail.com>
2022-09-07 18:35:21 +0300

    task1 did

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("System.Boolean:\nMin value = {0}\nMax value = {1}\n", Boolean.FalseString, Boolean.TrueString);
+        Console.WriteLine("System.Char:\nMin value = {0}\nMax value = {1}\n", Char.MinValue, Char.MaxValue);
+        Console.WriteLine("System.Decimal:\nMin value = {0}\nMax value = {1}\n", Decimal.MinValue, Decimal.MaxValue);
+        Console.WriteLine("System.DateTime:\nMin value = {0}\nMax value = {1}\n", DateTime.MinValue, DateTime.MaxValue);
+        Console.WriteLine("System.Double:\nMin value = {0}\nMax value = {1}\n", Double.MinValue, Double.MaxValue);
+        Console.WriteLine("System.Single:\nMin value = {0}\nMax value = {1}\n", Single.MinValue, Single.MaxValue);
+        Console.WriteLine("System.Byte:\nMin value = {0}\nMax value = {1}\n", Byte.MinValue, Byte.MaxValue);
+        Console.WriteLine("System.SByte:\nMin value = {0}\nMax value = {1}\n", SByte.MinValue, SByte.MaxValue);
+        Console.WriteLine("System.Int16:\nMin value = {0}\nMax value = {1}\n", Int16.MinValue, Int16.MaxValue);
+        Console.WriteLine("System.Int32:\nMin value = {0}\nMax value = {1}\n", Int32.MinValue, Int32.MaxValue);
+        Console.WriteLine("System.Int64:\nMin value = {0}\nMax value = {1}\n", Int64.MinValue, Int64.MaxValue);
+        Console.WriteLine("System.UInt16:\nMin value = {0}\nMax value = {1}\n", UInt16.MinValue, UInt16.MaxValue);
+        Console.WriteLine("System.UInt32:\nMin value = {0}\nMax value = {1}\n", UInt32.MinValue, UInt32.MaxValue);
+        Console.WriteLine("System.UInt64:\nMin value = {0}\nMax value = {1}\n", UInt64.MinValue, UInt64.MaxValue);
+
