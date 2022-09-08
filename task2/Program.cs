9464c8c18c6aea6fd89de073d80c853098e6fc43
denjasan <denjasan@gmail.com>
2022-09-08 11:29:45 +0300

    Task 2 done

---
+++ task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+internal class Rectangle
+{
+    private double side1 = 0;
+    private double side2 = 0;
+
+    public Rectangle(double sideA, double sideB)
+    {
+        this.side1 = sideA;
+        this.side2 = sideB;
+
+    }
+
+    private double AreaCalculator()
+    {
+        return side1 * side2;
+    }
+
+    private double PerimeterCalculator()
+    {
+        return side1 * 2 + side2 * 2;
+    }
+
+    public double Area
+    {
+        get
+        {
+            return AreaCalculator();
+        }
+    }
+
+    public double Perimeter
+    {
+        get
+        {
+            return PerimeterCalculator();
+        }
+    }
+    
+}
+
+internal static class Test {
+    private static void Main()
+    {
+        Console.Write("a = ");
+        var a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("b = ");
+        var b = Convert.ToDouble(Console.ReadLine());
+        var rec = new Rectangle(a, b);
+        Console.WriteLine($"Area = {rec.Area}\nPerimeter = {rec.Perimeter}");
+
+    }
+}
+
